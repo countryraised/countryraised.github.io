a78d73b001f19ee21fdbdf7f24379e46b315a6e1
vincentm <matthiasvincentm@johndeere.com>
2021-01-13 08:34:45 -0600

    reset button and wins counter work

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // container and block selectors
 const container = document.querySelectorAll(".container");
-
 const reset = document.querySelector(".reset");
 
 // store selected block and containers from and to
 let choice ;   //selected block
 let fromCont ;     //1st selcted container
 let toCont ;    //2nd selected destination container
-// let winCount = 0;
-// game starts when page refreshed?
+let winCount = 0;
+// game starts when page loaded
 document.onload = start();
 
 // containers need event listeners.
@@ -17,9 +16,6 @@ function start(){
         container[i].addEventListener("click", chooseContainer);
     }  
 };
-
-// selected block
-
 
 // choose starting block container  || had all working unless user clicks on empty container first.
 function chooseContainer(evt){
@@ -73,8 +69,8 @@ function win(){
         console.log("You win.");
         let winner = document.querySelector(".win");
         winner.innerHTML = "YOU WIN!!!";
-        // winCount++;
-                        
+        winCount++;
+        document.querySelector(".winCnt").innerHTML = `Wins = ${winCount}!` ;
     }
     else{
         return
@@ -82,7 +78,6 @@ function win(){
 }
 
 reset.addEventListener("click", function(){
-    // location.reload();
     resetBoard()
 });
 
