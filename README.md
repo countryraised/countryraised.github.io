ee69ab8a1d07498e2cfd2e8f11b184eb0744af2d
vincentm <matthiasvincentm@johndeere.com>
2021-01-13 07:57:06 -0600

    cleaning up code and adding info to readMe

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Project1-game
 Attempting the Tower of Hanoi game
 
-Pussle strategy game of moving blocks in order to another position.
+Puzzle strategy game of moving blocks in order to another position.
 
 I used Javascript, css, and html.
 
 
 MVP to work.
+
 User should be able to to move top block from 1 column to next.
 
 User should only be able to move top block. 
 
-User should only be able to place block on top of a larger block.
+User should only be able to place block on top of a larger block.(found a condition this fails)
 
 Inform user of Win.
 
@@ -25,3 +26,34 @@ track wins.
 change difficulty by adding or removing blocks.
 
 
+things I tried along the way
+
+Putting event listeners on the blocks at the same time as the containers. 
+This caused issues with clicking on a block did not trigger the event on the container also.
+
+const block = document.querySelectorAll(".block");
+// blocks need event listeners.
+    // for(i=0; i<5; i++){
+    //     block[i].addEventListener("click", chooseBlock);
+    //     console.log(block[i]);
+    // }
+
+// function chooseBlock(e){
+//     if (choice = null){
+//         choice = e.target;
+//         choice.style.background = blue;
+//         console.log("block " + choice);
+//     }
+// };
+
+
+Lots of goodle searches for Tower of Hanoi deal with recursion, almost all of them are talking about solving the problem rather than creating the game. Recursion maybe could have been used in place of my for loop to add event listeners. 
+
+    // Recursion. I believe this allows completion of the game without moving peices individually?
+//moveTower(disk, source, dest, spare);  for:: disk=5 source=A dest=B spare=C
+//  If disk == 0 then: move disk from source to destination.
+//  Else 
+//      MoveTower(disk -1, souece, spare, dest)
+//      move disk from source to destination
+//      MoveTower(disk-1), spare, dest, source)
+//  End If
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // container and block selectors
 const container = document.querySelectorAll(".container");
-const block = document.querySelectorAll(".block");
+
 const reset = document.querySelector(".reset");
 
 // store selected block and containers from and to
 let choice ;   //selected block
 let fromCont ;     //1st selcted container
 let toCont ;    //2nd selected destination container
-let original= document.querySelector(".gamecontainer").innerHTML;
-let winCount = 0;
+// let winCount = 0;
 // game starts when page refreshed?
 document.onload = start();
 
@@ -16,31 +15,17 @@ document.onload = start();
 function start(){
     for(i=0; i<3; i++){
         container[i].addEventListener("click", chooseContainer);
-        console.log(container[i]);
-    }  // blocks need event listeners.
-    // for(i=0; i<5; i++){
-    //     block[i].addEventListener("click", chooseBlock);
-    //     console.log(block[i]);
-    // }
+    }  
 };
 
 // selected block
-// function chooseBlock(e){
-//     if (choice = null){
-//         choice = e.target;
-//         choice.style.background = blue;
-//         console.log("block " + choice);
-//     }
-// };
+
 
 // choose starting block container  || had all working unless user clicks on empty container first.
 function chooseContainer(evt){
     if (fromCont == null && evt.target.querySelector(".block") !=null) {  
         fromCont = evt.target;
         choice = fromCont.querySelector(".block");
-        // console.log(`from ${fromCont.id}`);
-        // console.log(fromCont.firstChild);
-        // console.log(`block ${choice.id}`);
         choice.style.background = "blue";
         
     }
@@ -55,7 +40,6 @@ function chooseContainer(evt){
 };
 
 // compare chosen block to block in destination container
-// function compare()
 function compare(){
     if(toCont.querySelector(".block") == null) {  //( to container is empty, move block into container)
             toCont.appendChild(choice);
@@ -67,8 +51,7 @@ function compare(){
     else{      //compare choice vs toCont 's existing firstChild
         choiceWdth = choice.id.replace(/\D/g,"");      // turning pixel width to an integer
         toContWdth = toCont.querySelector(".block").id.replace(/\D/g,"");
-        // console.log(choiceWdth);
-        // console.log(toContWdth);
+        
         if( choiceWdth < toContWdth) { //then move chioce into container
             toCont.insertBefore(choice, toCont.firstChild);
             choice.style.background = "red";
@@ -90,6 +73,7 @@ function win(){
         console.log("You win.");
         let winner = document.querySelector(".win");
         winner.innerHTML = "YOU WIN!!!";
+        // winCount++;
                 
     }
     else{
@@ -99,29 +83,6 @@ function win(){
 
 reset.addEventListener("click", function(){
     location.reload();
-    // document.querySelector(".gamecontainer").innerHTML = original;
-    // // resetBoard();
-    // choice = null;
-    // fromCont = null;
-    // toCont =null;
-    // start();
 });
 
-// function resetBoard() {
-//     document.querySelector("#cntnTwo").innerHTML = "";
-//     document.querySelector("#cntnThree").innerHTML = "";
-//     document.querySelector("#cntnThree").innerHTML = "";
-//     createElement()
-// }
 
-
-
-
-// Recursion. I believe this allows completion of the game without moving peices individually?
-//moveTower(disk, source, dest, spare);  for:: disk=5 source=A dest=B spare=C
-//  If disk == 0 then: move disk from source to destination.
-//  Else 
-//      MoveTower(disk -1, souece, spare, dest)
-//      move disk from source to destination
-//      MoveTower(disk-1), spare, dest, source)
-//  End If
